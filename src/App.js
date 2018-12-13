c09c81703f69cf7ea9c820a804a67e8ff263a722
root <root@localhost.localdomain>
2018-12-13 13:02:29 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,115 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list=[
+        {
+                title: 'React',
+                url: 'https://reactjs.org',
+                author: 'Jordan Walke',
+                num_comments: 3,
+                points: 4,
+                objectID: 0,
+        },
+        {
+                title:'Redux',
+                url:'https://redux.js.org',
+                author:'Dan Ambramov, Andrew Clark',
+                num_comments:2,
+                points:5,
+                objectID:1,
+        }
+]
+
+// function isSearched(searchTerm){
+//         return function(item){
+//                 // some condition which returns true or false
+//                 return item.title.toLowerCase().includes(searchTerm.toLowerCase())
+//         }
+// }
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
 class App extends Component {
-  render() {
+        constructor(props){
+                super(props);
+
+                this.state ={
+                        list: list,
+                        searchTerm:"",
+                };
+                this.onDismiss = this.onDismiss.bind(this);
+                this.onSearchChange = this.onSearchChange.bind(this);
+        }
+        onDismiss(id){
+                const updatedList = this.state.list.filter((item)=>{return item.objectID !== id})
+                this.setState({list: updatedList})
+        }
+        onSearchChange(event){
+                this.setState({searchTerm: event.target.value})
+
+        }
+        render() {
+                const {searchTerm, list} = this.state;
+                const helloWorld = "Welcome to the Road to learn React";
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+                <Search 
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                />
+                <Table
+                        list={list}
+                        pattern={searchTerm}
+                        onDismiss={this.onDismiss}
+                />
         </header>
-      </div>
+                      </div>
     );
   }
 }
 
+class Search extends Component{
+        render(){
+                const {value, onChange, children} = this.props;
+                return(
+                        <form>
+                                {children} <input
+                                        type="text"
+                                        value={value}
+                                        onChange={onChange}/>
+                        </form>
+                )
+        }
+}
+
+class Table extends Component{
+        render(){
+                const {list, pattern, onDismiss} = this.props;
+                return(
+                        <div>
+                {list.filter(isSearched(pattern)).map(item => {
+                        return(
+                                <div key={item.objectID}>
+                                        <span>
+                                                <a href={item.url}>{item.title}</a>
+                                        </span>
+                                        <span>{item.author}</span>
+                                        <span>{item.num_comments}</span>
+                                        <span>{item.points}</span>
+                                        <span>
+                                                <button
+                                                        onClick={()=> onDismiss(item.objectID)}
+                                                        type="button"
+                                                >
+                                                        Dismiss
+                                                </button>
+                                        </span>
+                                </div>
+                        )
+                })}
+                        </div>
+);
+        }
+}
 export default App;
