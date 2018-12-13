93641a5bd6a03c42083bc846efa4156be9da29e5
rancore <rancore@localhost.localdomain>
2018-12-13 13:56:46 +0530

    Updates

---
+++ precache-manifest.a004f9fd80ae34b5153a9e6ab0fadb7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "0013ba3700a2e55f18c5",
+    "url": "/hackernews/static/js/main.0013ba37.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/hackernews/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "0013ba3700a2e55f18c5",
+    "url": "/hackernews/static/css/main.1395694d.chunk.css"
+  },
+  {
+    "revision": "187c289e965d70d0377df5508f50826a",
+    "url": "/hackernews/index.html"
+  }
+];
